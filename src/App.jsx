91d92c9ebd760f4c0f0dd9b4daca3b1d342d4601
Tanya <tbansal535@gmail.com>
2025-02-06 12:01:29 +0530

    first react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1> hello react appp Deploy on EC2 with AWS </h1> </div>
+  )}
+
+export default App
